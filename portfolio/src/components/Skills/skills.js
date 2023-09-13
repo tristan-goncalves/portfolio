19f257410dec5609b437dd45d8c7e2aabf268ff8
TrixJoestar <tristan.goncalves25@gmail.com>
2023-09-13 17:13:44 +0200

    Fin de la partie Compétences

--- portfolio/src/components/Skills/skills.js
+++ portfolio/src/components/Skills/skills.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./skills.css";
-import UIDesign from "../../assets/ui-design.png"
-import WebDesign from "../../assets/website-design.png"
-import AppDesign from "../../assets/app-design.png"
+import DEVPython from "../../assets/python-logo.png"
+import DEVWeb from "../../assets/html-css-logo.png"
+import CyberSecuriy from "../../assets/cybersecurite-logo.png"
 
 const Skills = () => {
   return (
@@ -11,28 +11,28 @@ const Skills = () => {
         <span className='skillDesc'>Je suis un futur développeur, actuellement en alternance chez Kapptivate, j'ai les fondamentaux dans plusieurs langages tels que : C, C++, Python, Java, HTLM/CSS, Javacript.</span>
         <div className='skillBars'>
             <div className='skillBar'>
-                <img src={UIDesign} alt="UIDesign" className='skillBarImg'></img>
+                <img src={DEVPython} alt="DEVPython" className='skillBarImg'></img>
                 <div className='skillBarText'>
                     <h2>Développement Python</h2>
-                    <p></p>
+                    <p>J'ai plusieurs expériences en développement Python, nottament une application qui consistait à analyser du code C++ à l'aide de Python afin de pouvoir corriger automatiquement des codes d'étudiants débutants.</p>
                 </div>
             </div>
         </div>
         <div className='skillBars'>
             <div className='skillBar'>
-                <img src={WebDesign} alt="WebDesign" className='skillBarImg'></img>
+                <img src={DEVWeb} alt="DEVWeb" className='skillBarImg'></img>
                 <div className='skillBarText'>
                     <h2>Développement Web</h2>
-                    <p></p>
+                    <p>J'ai participé à la création d'un site de recherches de mangas. Plus précisément à la page vitrine qui mettait en valeur plusieurs mangas afin de donner envie à l'utilisateur d'en savoir plus sur nos services.</p>
                 </div>
             </div>
         </div>
         <div className='skillBars'>
             <div className='skillBar'>
-                <img src={AppDesign} alt="AppDesign" className='skillBarImg'></img>
+                <img src={CyberSecuriy} alt="CyberSecuriy" className='skillBarImg'></img>
                 <div className='skillBarText'>
-                    <h2></h2>
-                    <p></p>
+                    <h2>Cybersécurité</h2>
+                    <p>Je suis actuellement en Bachelor 3ème année en Cybersécurité. Je commence à me faire la main dès maintenant avec ce portfolio.</p>
                 </div>
             </div>
         </div>
